07093528efbe854f46adcadcc02e5675c9985c8d
huchengyu123 <55024374+huchengyu123@users.noreply.github.com>
2019-09-21 23:00:10 +0800

    Update Sudoku.java

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,16 +11,16 @@ import java.util.ArrayList;
 import java.util.Random;
  
 public class Sudoku {
-
-   
- 
+	
+	   /*
+     * 初始化命令行参数
+     */
+	public static String inputFilename;
+	public static String outputFilename;
+	public static int m;
+	public static int n;
     /**
      * 判断在九宫格中的坐标(x,y)的位置上插入value，是否符合规则
-     *
-     * @param x
-     * @param y
-     * @param value
-     * @return
      */
     public static Boolean legal(int a[][],int x, int y, int value,int m) {
  
@@ -51,7 +49,7 @@ public class Sudoku {
             }
         }
         if(m==4){
-            //(minX,minY)是(x,y)所属小九宫格的左上角的坐标
+            //(minX,minY)是(x,y)所属小4宫格的左上角的坐标
             int minX = x / 2 * 2;
             int minY = y / 2 * 2;
 
@@ -65,7 +63,7 @@ public class Sudoku {
             }
         }
         if(m==8){
-            //(minX,minY)是(x,y)所属小九宫格的左上角的坐标
+            //(minX,minY)是(x,y)所属小8宫格的左上角的坐标
             int minX = x / 4 * 4;
             int minY = y / 2 * 2;
      
@@ -79,7 +77,7 @@ public class Sudoku {
             }
         }
         if(m==6){
-            //(minX,minY)是(x,y)所属小九宫格的左上角的坐标
+            //(minX,minY)是(x,y)所属小宫格的左上角的坐标
             int minX = x / 2 * 2;
             int minY = y / 3 * 3;
      
@@ -99,9 +97,7 @@ public class Sudoku {
 
     //第三部分，是针对第二部分生成的数独游戏，使用回溯法，实现对数独的解答。
     //shuDu[][]是用来存放数独游戏的二维数组。
-    //numOfSolution是用来统计shuDu[][]中存放的数独游戏的解的个数
     public static int shuDu[][] = new int[9][9];
-    public static int numOfSolution=1;
     public static void setShuDu(int[][] shuDu) {
         Sudoku.shuDu = shuDu;
     }
@@ -109,46 +105,27 @@ public class Sudoku {
     /**
      * 回溯法求解数独，参考第一部分用回溯法随机生成数独的解空间的代码
      * @param k
+     * @throws IOException 
      */
-    public static void shuDu_solution(int k,int m) {
+    public static void shuDu_solution(int k,int m) throws IOException {
         if (k == (m*m)) {
-            System.out.println("解法"+numOfSolution);
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < m; j++) {
-                    System.out.print(shuDu[i][j] + " ");
-                }
-                System.out.println();
-            }
-            
-             //     写入out.txt文件  
-            try { 
-            	
-            String outfile="D:\\sudoku\\out.txt";
-            File writename = new File(outfile); // 相对路径，如果没有则要建立一个新的out.txt文件  
-            writename.createNewFile(); // 创建新文件  
-            BufferedWriter out = new BufferedWriter(new FileWriter(writename));  
-            
-            //将数组中的数据写入到文件中。每行各数据之间TAB间隔
+          String src= "D:\\sudoku\\"+outputFilename;
+            try{
+            FileWriter fw = new FileWriter(src,true);
             for(int i=0;i<m;i++){
-                for(int j=0;j<m;j++){
-              
-                    out.write(shuDu[i][j]+" ");
-                   
+                for(int j=0;j<m;j++){ 
+                	fw.write(shuDu[i][j]+" ");
+
                 }
 
-                out.write("\r\n");
+                fw.write("\r\n");
             }
-            out.flush(); // 把缓存区内容压入文件  
-            out.close(); // 最后记得关闭文件  
+            fw.write("\r\n");
+            fw.close(); // 最后记得关闭文件  
             }
             catch (Exception e) {  
                 e.printStackTrace();  
             }  
-            
-            
-            
- 
-            numOfSolution++;
             return;
         }
         int x = k / m;
@@ -165,66 +142,68 @@ public class Sudoku {
             shuDu_solution(k + 1,m);
         }
     }
- 
-    //打印二维数组a[m][n]
-    public static void displayArray(int a[][], int m, int n) {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
+
+    public static void loadArgs(String args[]){
+    	if(args.length>0&&args!=null){
+    		for(int i=0;i<args.length;i++){
+    			switch (args[i]) {
+				case "-i":
+					inputFilename = args[++i];
+					break;
+				case "-o": 
+					outputFilename = args[++i];
+					break;
+				case "-m": 
+					m=Integer.valueOf(args[++i]);
+					break;
+				case "-n":
+					n=Integer.valueOf(args[++i]);
+				    break;
+
+				default:
+					break;
+				}
+    		}
+    	}
     }
- 
+
     public static void main(String[] args) throws IOException {
     	
-    	int m =8;
-    	int generateShuDu[][]=new int[9][9];   
-		String path = "D:\\sudoku";
-		String filename = "input8.txt";
-		/*String filename = args[0];*/
-		
-		File myFile = new File(path,filename);
+    	loadArgs(args);
+    	int generateShuDu[][]=new int[10][10];   
+		File myFile = new File("D:\\sudoku",inputFilename);
 		Reader reader = new InputStreamReader(new FileInputStream(myFile),"UTF-8"); 
 
-		// 一次读一个字节
 		int tempchar;  
 		int i=0;
 		int j=0;
-	
 		    while ((tempchar = reader.read()) != -1) {  
 		    if ( (((char) tempchar) != '\n') &&(((char) tempchar) != ' ')) {  
 		        if(i<m){
 		        	if(j<m){
-		        		generateShuDu[i][j]=((char) tempchar)-48;
-		        		j++;
-		        	}else{	     
+		        		if(tempchar!=13){
+		        			generateShuDu[i][j]=((char) tempchar)-48;
+			        		j++;
+		        		}
+		        	}else{	
 		        		i++;
 		        		j=0;
 		        		generateShuDu[i][j]=((char) tempchar)-48;
 		        	}
+		        }
+		        if(i==m){
+		        	while(n!=0){
+			            setShuDu(generateShuDu);
+			            shuDu_solution(0,m);
+			            n--;
+			            i=0;j=0;
+		        	}
 		        	
 		        }
-		 /*       if(i==m){
-		        	System.out.println("input.txt:\n");
-		            setShuDu(generateShuDu);
-		            displayArray(shuDu, m, m);
-
-		            System.out.println("out.txt:\n");
-		            shuDu_solution(0,m);
-		            i=0;j=0;
-		        }*/
 		    }  
 		}
 		reader.close();
-       
-
-        System.out.println("读出数独文本");
-        setShuDu(generateShuDu);
-        displayArray(shuDu, m, m);
-
-        System.out.println("求解");
-        shuDu_solution(0,m);
+        
     }
     
 }
